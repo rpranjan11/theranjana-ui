e720adb13f4221320c7ba8719ef9890d969b3d56
Ranjan [란잔] <ranjan.rp@timbel.net>
2024-11-27 19:14:07 +0900

    feats-social_customs - initial data updated

---
+++ src/data/social_customs/predefinedMessages.js
@@ -0,0 +1,31 @@
+import wel_img from '../../images/social_customs/wel_img.png';
+
+const predefinedMessages = [
+    { type: 'text', content: '킹 선거 과정에 오신 것을 환영합니다     <=>     Welcome to the King Election Process' },
+    { type: 'image', content: wel_img },
+    { type: 'text', content: 'Today\'s topic is as follows:     <=>     오늘의 주제는 다음과 같습니다:' },
+    { type: 'text', content: 'Are social customs a waste of time?    <=>     사회적 관습은 시간 낭비일까요?' },
+    { type: "image", content: wel_img},
+    { type: 'text', content: 'Have you ever attended a wedding ceremony or a cultural festival that felt more like a marathon than a celebration or long enough to be called a boring event?     <=>     ' },
+    { type: "image", content: wel_img},
+    { type: 'text', content: 'Social customs are ingrained in the fabric of every culture, yet they can often feel outdated or irrelevant. Are they truly a waste of time, or do they serve a deeper purpose?     <=>     ' },
+    { type: "text", content: 'What is Social Customs?    <=>     사회적 관습이란 무엇인가요?' },
+    { type: "text", content: 'Collective traditions, practices, and rituals that societies follow to maintain social harmony and identity. Example: Namaste in India symbolizes respect and humility, while the bow in Korea reflects honor and etiquette.     <=>     '},
+    { type: "image", content: wel_img},
+    { type: "text", content: 'Arguments Against Social Customs    <=>     사회적 관습에 반대하는 주장들' },
+    { type: "text", content: 'India :: Social customs like elaborate multi-day wedding ceremonies and caste-based traditions can be time-consuming, financially burdensome, and often clash with the values of a modern, progressive society.\n' +
+            'South Korea :: Traditional customs such as Seollal preparations and Confucian practices, including gender-specific roles in rituals, can be overly demanding, both in time and resources, making them less practical in today’s fast-paced and egalitarian world.'},
+    { type: "text", content: 'Arguments for Social Customs    <=>     사회적 관습에 대한 주장들' },
+    { type: "text", content: 'India :: Social customs, like festivals such as Diwali and practices like joint family gatherings or rituals, help preserve cultural identity, foster community bonds, and provide spiritual reflection and mindfulness.\n' +
+            'South Korea :: Traditional customs, including celebrations like Chuseok and ancestral rites like Jesa, strengthen family and societal ties, uphold cultural heritage, and offer a sense of purpose and connection to one’s roots.'},
+    { type: "image", content: wel_img},
+    { type: "text", content: 'My perspective for social customs    <=>     ' },
+    { type: "text", content: 'Social customs aren’t inherently a waste of time; their relevance depends on perspective. Simplified cultural ceremonies that retain core traditions without excess effort will help to achieve a balancing modern lifestyles with Confucian values.    <=>     '},
+    { type: "text", content: 'Social customs are not black or white but they are greyish. It might sometimes feel burdensome, but they are the threads that weave the rich tapestry of human culture.     <=>     '},
+    { type: "image", content: wel_img},
+    { type: 'text', content: 'Feel free to share your thoughts on the topic.    <=>     ' },
+    { type: 'text', content: 'Thank you for your participation.    <=>     참여해 주셔서 감사합니다.'},
+    { type: 'text', content: 'ThankYou | 감사합니다' }
+];
+
+export default predefinedMessages;
