580d3d6cf86d2fc4a51c6f8f1e012ae7abc74683
Ranjan Ram Pratap <rpranjan11@gmail.com>
2024-12-06 18:09:24 +0900

    Updated the routing in the App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppProvider } from './AppContext'; // Correct path to Context
 import AdminPage from './AdminPage';
 import ClientPage from './ClientPage';
 
 function App() {
     return (
-        <Router>
-          <Switch>
-            <Route path="/admin" component={AdminPage} />
-            <Route path="/audience" component={ClientPage} />
-          </Switch>
-        </Router>
+        <AppProvider>
+            <Router>
+                <Routes>
+                    <Route path="/admin" element={<AdminPage />} />
+                    <Route path="/audience" element={<ClientPage />} />
+                </Routes>
+            </Router>
+        </AppProvider>
     );
 }
 
