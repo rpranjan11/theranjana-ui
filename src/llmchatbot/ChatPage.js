ffa82189d69fb4274057cc4ebe4c1d1336f3c087
Ranjan Ram Pratap [란잔] <rpranjan11@gmail.com>
2024-12-24 17:39:03 +0900

    feats_v2-llmchatbot - modified ChatPage to correctly target the apis

--- src/llmchatbot/ChatPage.js
+++ src/llmchatbot/ChatPage.js
@@ -10,21 +10,25 @@ export const ChatPage = () => {
     const llmchatbotServiceDomain = process.env.REACT_APP_LLMCHATBOT_API_URL;
     const [file, setFile] = useState("")
     const [fileName, setFileName] = useState("")
+    const [selectedChatGptModel, setSelectedChatGptModel] = useState("gpt-3.5-turbo");
+    const [selectedOllamaModel, setSelectedOllamaModel] = useState("llama3.2");
+    const [chatGptQueryType, setChatGptQueryType] = useState("");
+    const [ollamaQueryType, setOllamaQueryType] = useState("");
 
     const textRef = useRef(null);
     const formRef = useRef(null);
 
     const [fileText, setFileText] = useState("")
 
-    const [que1, setQue1] = useState("")
-    const [que2, setQue2] = useState("")
+    const [chatGptQuery, setChatGptQuery] = useState("")
+    const [ollamaQuery, setOllamaQuery] = useState("")
 
-    const [queArr1, setQueArr1] = useState([])
-    const [ansArr1, setAnsArr1] = useState([])
+    const [chatGptQueryArray, setChatGptQueryArray] = useState([])
+    const [chatGptAnswerArray, setChatGptAnswerArray] = useState([])
     const [text1, setText1] = useState("")
 
-    const [queArr2, setQueArr2] = useState([])
-    const [ansArr2, setAnsArr2] = useState([])
+    const [ollamaQueryArray, setOllamaQueryArray] = useState([])
+    const [ollamaAnswerArray, setOllamaAnswerArray] = useState([])
     const [text2, setText2] = useState("")
 
     const scrollToBottom = () => {
@@ -53,13 +57,15 @@ export const ChatPage = () => {
 
     const handleUploadPdf = () => {
         const formData = new FormData();
-        formData.append('pdf', file); 
+        formData.append('pdf', file);
+        formData.append('chatgpt_model', selectedChatGptModel);
+        formData.append('ollama_model', selectedOllamaModel);
         
         axios.post(`${llmchatbotServiceDomain}/uploadpdf`, formData)
             .then(response => {
                 console.log(response.data);
-                setText1(response.data.chatgptresponse)
-                setText2(response.data.ollamaresponse)
+                setText1(response.data.chatgptResponse)
+                setText2(response.data.ollamaResponse)
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -67,43 +73,53 @@ export const ChatPage = () => {
     };
 
 
-    const handleQuestions1 = (text) => {
-        setQueArr1((prev) => [...prev, text])
+    const handleChatGptQueries = (text, queryType) => {
+        setChatGptQueryArray((prev) => [...prev, text])
         // model = "openAPI"
-        const url = `${llmchatbotServiceDomain}/getchatresponse/${text}`;
+        let url = "";
+        if(queryType === "pdf_based"){
+            url = `${llmchatbotServiceDomain}/chatgptcustomresponse/${selectedChatGptModel}/${text}`;
+        }
+        else {
+            url = `${llmchatbotServiceDomain}/chatgptgenericresponse/${selectedChatGptModel}/${text}`;
+        }
 
         axios.get(url)
             .then(response => {
                 console.log(response.data)
-                setAnsArr1((prev) => [...prev, response.data.chatgptResponse])
+                setChatGptAnswerArray((prev) => [...prev, response.data.chatgptResponse])
                 scrollToBottom(); // Scroll to bottom after adding question
-
-                
             })
             .catch(error => {
                 console.error('Error:', error);
             });
 
-        setQue1("")
+        setChatGptQuery("")
 
     }
 
-    const handleQuestions2 = (text) => {
-        setQueArr2((prev) => [...prev, text])
+    const handleOllamaQueries = (text, queryType) => {
+        setOllamaQueryArray((prev) => [...prev, text])
         // model = "ollama"
-        const url = `${llmchatbotServiceDomain}/getllmresponse/${text}`;
+        let url = "";
+        if(queryType === "pdf_based"){
+            url = `${llmchatbotServiceDomain}/ollamacustomresponse/${selectedOllamaModel}/${text}`;
+        }
+        else {
+            url = `${llmchatbotServiceDomain}/ollamagenericresponse/${selectedOllamaModel}/${text}`;
+        }
 
         axios.get(url)
             .then(response => {
                 console.log('Response:', response.data);
-                setAnsArr2((prev) => [...prev, response.data.ollamaresponse])
+                setOllamaAnswerArray((prev) => [...prev, response.data.ollamaResponse])
                 scrollToBottom(); // Scroll to bottom after adding question
             })
             .catch(error => {
                 console.error('Error:', error);
             });
 
-        setQue2("")
+        setOllamaQuery("")
     }
 
     const onClickCross = () => {
@@ -137,60 +153,97 @@ export const ChatPage = () => {
                             {file && <button id="close-btn" onClick={onClickCross}><i className="bi bi-x-lg"></i></button>}
                         </div>
                     </div>
-                   {text1 && <div style={{flexGrow:'1',display:'flex'}}>
+                    <div style={{flexGrow:'1',display:'flex'}}>
                         <Row style={{width:'100%'}}>
-                            <Col md={6} lg={6} className="px-3 pb-4">
+                            <Col md={6} lg={6} className="px-3 pb-4" style={{border:'1px solid #ccc'}}>
+                                <div style={{display: 'flex', alignItems: 'center'}}>
+                                    <label htmlFor="chatgpt-model" style={{marginRight: '10px'}}>Select ChatGpt
+                                        Model:</label>
+                                    <select id="chatgpt-model" style={{flex: 1}} value={selectedChatGptModel}
+                                            onChange={(e) => setSelectedChatGptModel(e.target.value)}>
+                                        <option value="gpt-3.5-turbo" selected>gpt-3.5-turbo</option>
+                                    </select>
+                                </div>
                                 <div className="card-content">
                                     <div className="text" id="text-area" ref={textRef}>
-                                        {text1 && <span>{text1}</span>}
+                                    {text1 && <span>{text1}</span>}
                                         {
-                                            queArr1.map((item, idx) => {
+                                            chatGptQueryArray.map((item, idx) => {
                                                 return (
-                                                    ansArr1[idx] && <div key={idx} style={{ display: 'flex', flexDirection: 'column' }}>
+                                                    chatGptAnswerArray[idx] &&
+                                                    <div key={idx} style={{display: 'flex', flexDirection: 'column'}}>
                                                         <span id="questions">-{item} </span>
-                                                        <span id={idx!==ansArr1.length-1?"answers":'latest'} >{ansArr1[idx]}</span>
+                                                        <span
+                                                            id={idx !== chatGptAnswerArray.length - 1 ? "answers" : 'latest'}>{chatGptAnswerArray[idx]}</span>
                                                     </div>
                                                 )
                                             })
                                         }
                                     </div>
-                                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <input className="input_text" type="text" value={que1} onChange={(e) => {
-                                            setQue1(e.target.value)
+                                    <div style={{display: 'flex', alignItems: 'center'}}>
+                                        <input className="input_text" type="text" value={chatGptQuery} onChange={(e) => {
+                                            setChatGptQuery(e.target.value)
                                         }}></input>
-                                        <i style={{ fontSize: '25px', cursor: 'pointer' }} onClick={() => handleQuestions1(que1)} className="bi bi-box-arrow-in-up px-3"></i>
+                                        <div style={{display: 'flex', alignItems: 'center'}}>
+                                            <select id="chatgpt-query-type" value={chatGptQueryType}
+                                                    onChange={(e) => setChatGptQueryType(e.target.value)}>
+                                                <option value="pdf_based" selected>From Pdf</option>
+                                                <option value="general">General</option>
+                                            </select>
+                                            <i style={{fontSize: '25px', cursor: 'pointer'}}
+                                               onClick={() => handleChatGptQueries(chatGptQuery, chatGptQueryType)}
+                                               className="bi bi-box-arrow-in-up px-3"></i>
+                                        </div>
                                     </div>
                                 </div>
                             </Col>
-                            <Col md={6} lg={6} className="px-3 pb-4">
+                            <Col md={6} lg={6} className="px-3 pb-4" style={{border: '1px solid #ccc'}}>
+                                <div style={{display: 'flex', alignItems: 'center'}}>
+                                    <label htmlFor="ollama-model" style={{marginRight: '10px'}}>Select Ollama
+                                        Model:</label>
+                                    <select id="ollama-model" style={{flex: 1}} value={selectedOllamaModel}
+                                            onChange={(e) => setSelectedOllamaModel(e.target.value)}>
+                                        <option value="llama3.2" selected>Llama 3.2</option>
+                                        <option value="orca-mini">Orca Mini</option>
+                                    </select>
+                                </div>
                                 <div className="card-content">
                                     <div className="text">
                                         {text2 && <span>{text2}</span>}
                                         {
-                                            queArr2.map((item, idx) => {
+                                            ollamaQueryArray.map((item, idx) => {
                                                 return (
-                                                    ansArr2[idx] && <div key={idx} style={{ display: 'flex', flexDirection: 'column' }}>
+                                                    ollamaAnswerArray[idx] &&
+                                                    <div key={idx} style={{display: 'flex', flexDirection: 'column'}}>
                                                         <span id="questions">{item} </span>
-                                                        <span id="answers">{ansArr2[idx]} </span>
+                                                        <span id="answers">{ollamaAnswerArray[idx]} </span>
                                                     </div>
                                                 )
                                             })
                                         }
                                     </div>
-                                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <input className="input_text" type="text" value={que2} onChange={(e) => {
-                                            setQue2(e.target.value)
+                                    <div style={{display: 'flex', alignItems: 'center'}}>
+                                        <input className="input_text" type="text" value={ollamaQuery} onChange={(e) => {
+                                            setOllamaQuery(e.target.value)
                                         }}></input>
-                                        <i style={{ fontSize: '25px', cursor: 'pointer' }} onClick={() => handleQuestions2(que2)} className="bi bi-box-arrow-in-up px-3"></i>
+                                        <div style={{display: 'flex', alignItems: 'center'}}>
+                                            <select id="ollama-query-type" value={ollamaQueryType}
+                                                    onChange={(e) => setOllamaQueryType(e.target.value)}>
+                                                <option value="pdf_based" selected>From Pdf</option>
+                                                <option value="general">General</option>
+                                            </select>
+                                            <i style={{fontSize: '25px', cursor: 'pointer'}}
+                                               onClick={() => handleOllamaQueries(ollamaQuery, ollamaQueryType)}
+                                               className="bi bi-box-arrow-in-up px-3"></i>
+                                        </div>
                                     </div>
                                 </div>
-
                             </Col>
                         </Row>
-                    </div>}
+                    </div>
                 </div>
             </div>
-            <Footer />
+            <Footer/>
         </div>
     );
 };
