6c1e8b9d44932135bda207bbb860677c7a17ff5e
Ranjan [란잔] <ranjan.rp@timbel.net>
2024-12-26 14:17:11 +0900

    feats_v2-geographic-diversity-of-india - import useState in AdminComponent.js

--- src/confer/admin/AdminComponent.js
+++ src/confer/admin/AdminComponent.js
@@ -1,5 +1,5 @@
 // src/AdminComponent.js
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../AppContext';
 
 const AdminComponent = () => {
